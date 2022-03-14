b1ae6ac61c62fc85f18e6f7080b13e261f985c3d
GuiMoura18 <100542105+GuiMoura18@users.noreply.github.com>
2022-03-14 03:14:41 -0300

    Commit Inicial

--- src/js/index.js
+++ src/js/index.js
@@ -23,25 +23,25 @@ const pokemonscard = document.querySelectorAll('.cartao-pokemon')
 listaSelecaopokemons.forEach(pokemon => {
     //vamos precisar trabalhar com um evento de clique feito pelo usuário na listagem de pokémons
     
-    pokemon.addEventListener('click, () => {
+    pokemon.addEventListener('click', () => {
         //remover a classe aberto só do cartão que está aberto
-        const cartaoPokemonAberto = document.querySelector('aberto')
+        const cartaoPokemonAberto = document.querySelector('.aberto')
         cartaoPokemonAberto.classList.remove('aberto')
 
         //ao clicar em um pokémon da listagem pegamos o id desse pokémon pra saber qual cartão mostrar
         const idPokemonSelecionado = pokemon.attributes.id.value
 
-        const idcartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+        const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
         const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
         cartaoPokemonParaAbrir.classList.add('aberto')
 
         //remover a classe ativo que marca o pokémon selecionado
-        const pokemonAtivoNaListagem = document.querySelector('ativo')
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
         pokemonAtivoNaListagem.classList.remove('ativo')
 
         //adicionar a classe ativo no item da lista selecionado
         const idPokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
-        pokemonSelecionadoNaListagem.classList.add('ativo')
+        idPokemonSelecionadoNaListagem.classList.add('ativo')
 
     })
 }) 
